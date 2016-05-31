46f76e79ce5b14da6491a2c4d3add290a81141f2
lynnsamuelson <lynn.samuelson@gmail.com>
2016-05-31 09:20:03 -0500

    Template for web api added

---
+++ WebApi_Template/src/WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Linq;
+using System.Web.Http;
+using Microsoft.AspNet.Mvc;
+
+using WebApi_Template.Models;
+
+namespace WebApi_Template.Controllers
+{
+  
+    public class HomeController 
+    {
+        [HttpGet]
+        public string GetAll()
+        {
+                return "This is GetAll in ModelOne";
+        }
+    }
+}
---
+++ WebApi_Template/src/WebApplication4/Models/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Data.Entity;
+using Microsoft.Extensions.Configuration;
+
+
+
+
+namespace WebApi_Template.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+ 
+        public DbSet<ModelOne> ModelOne { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+       
+    }
+}
