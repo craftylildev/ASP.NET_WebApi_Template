306fbc45f19111818c3a9c81d6583dbb2ba961b5
Jen Solima <jensolima@gmail.com>
2016-06-01 13:37:30 -0500

    added search artist phrase

--- WebApi_Template/src/WebApplication4/Controllers/HomeController.cs
+++ WebApi_Template/src/WebApplication4/Controllers/HomeController.cs
@@ -17,7 +17,6 @@ namespace WebApi_Template.Controllers
             dbContext = context;
         }
 
-
         [HttpGet]
         public IEnumerable<Album> GetAll()
         {
@@ -25,17 +24,29 @@ namespace WebApi_Template.Controllers
             return products;
         }
 
-
+        // Bring back 100 artists and order by name (hint new model class, new entry to the entityset in the DBContext class)
         [HttpGet]
         public IEnumerable<Artist> GetTopArtist()
         {
-            var products = dbContext.Artist.Take(100).ToList();
-            IEnumerable<Artist> artist = products.OrderBy(a => a.Name);
-                
-            return products;
+            // WHY IS THIS NOT WORKING
+            //var products = dbContext.Artist.Take(100).ToList();
+            //IEnumerable<Artist> artist = products.OrderBy(a => a.Name);
+            //return products;
+
+            return dbContext.Artist.Take(100).ToList().OrderBy(a => a.Name);
         }
 
-       
+        // Pass in a search phase to the Artist table, query the artist table by the search phrase, output the results.
+        [HttpGet]
+       public IEnumerable<Artist> SearchArtist(string artistName)
+        {
+            //var products = dbContext.Artist.Where(a => a.Name == artistName).ToList();
+            //return products;
+            
+            return dbContext.Artist.Where(a => a.Name == artistName).ToList();
+        }
+
+        // List all the artist on a particular album you like(hint, will need to pass an album id in the calling url to the action)
 
 
     }
